7d38b6750a013f027d2d5406562da453e02e1d25
Hieu Luong <minhhieudn98@gmail.com>
2025-09-18 03:12:09 +0700

    update router link when login and logout

--- Frontend/DatingWeb/src/layout/nav/nav.ts
+++ Frontend/DatingWeb/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,11 +12,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class Nav {
   protected accountService = inject(AccountService);
   protected creds: any = {}
+  private router = inject(Router);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (response) => {
-        console.log('Login successful', response);
+        this.router.navigateByUrl('/members');
         this.creds = {}; // Clear credentials after successful login
       },
       error: (error) => {
@@ -27,5 +28,6 @@ export class Nav {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
